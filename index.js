8208d0118535e590ec5b3a8649b92b7bb420fe92
westernfrog <haaamansingh007@gmail.com>
2022-07-02 11:44:59 +0530

    Updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $("#switch").click(function () {
   if (this.checked) {
     $("html").css("background-color", "#94B49F");
     $("html").css("transition", "3s");
-    /*window.location.href = "birthday.html";*/
+    window.location.href = "birthday.html";
   } else {
     $("html").css("background-color", "#4C3A51");
   }
